7c33b27f5d8d876762cced13345996ecf1575754
Luigi Balestrucci <card3001@outlook.it>
2024-03-27 19:32:21 +0100

    Ex 7 form React completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import { Login } from "./components/Login";
+import { UncontrolledLogin } from "./components/UncontrolledLogin";
+
+function App() {
+  return (
+    <>
+      <UncontrolledLogin />
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </>
+  );
+}
+
+export default App;
